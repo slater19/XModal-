76a023bca889f43fe3c53d6130aea8ac1295b20e
slater19 <sachinaggarwal19@gmail.com>
2025-01-21 12:19:20 +0530

    my changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import Xmodal from "./xmodal";
+
+
+// Create context
+
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+        <Xmodal />
+        {/* <ResultList /> */}
+      </div>
+    // </InputContext.Provider>
   );
 }
 
